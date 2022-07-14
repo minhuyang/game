2a8df7ecf212ce69c4355c88999f6979b03d4f0d
minhu yang <1552035834@qq.com>
2022-07-14 17:17:06 +0800

    modify game

--- game/views.py
+++ game/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(resquest):
+    line1 = '<h1 style = "text-align: center">这就是白燕！！！</h1>'
+    line3 = '<h1 style = "text-align: center">↓↓↓↓↓↓↓↓↓↓↓↓</h1>'
+    line2 = '<img src = "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fgss0.baidu.com%2F9vo3dSag_xI4khGko9WTAnF6hhy%2Fzhidao%2Fpic%2Fitem%2Fb999a9014c086e063c2ef9be09087bf40bd1cb45.jpg&refer=http%3A%2F%2Fgss0.baidu.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1660381320&t=3907b908181a3badc1bdd02386039ff1" width = 1000>'
+    return HttpResponse(line1 + line3 + line2)
