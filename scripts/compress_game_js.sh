3fd2aef654c93821d29175aead9adfcda6de9d60
minhu yang <1552035834@qq.com>
2022-08-05 20:25:17 +0800

    modify .sh

--- scripts/compress_game_js.sh
+++ scripts/compress_game_js.sh
@@ -6,5 +6,5 @@ JS_PATH_SRC=${JS_PATH}src/
 
 find $JS_PATH_SRC -type f -name '*.js' | sort | xargs cat | terser -c -m > ${JS_PATH_DIST}game.js
 
-echo yes | python3 /home/yangmh/acapp/manage.py collectstatic
+echo yes | python3 manage.py collectstatic
 
